f8551fd7bdbd0d31509c9c87fbcbd1ab3c036ce4
Lymar Yaroslav Victorovich <Lymar.Y.Vi@sberbank.ru>
2021-05-14 19:35:10 +0300

    commit

---
+++ src/main/java/interview/Introduction.java
@@ -0,0 +1,26 @@
+package interview;
+
+import java.util.Arrays;
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        double[] result = findAverages(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+        System.out.println("Averages of subarrays of size K: " + Arrays.toString(result));
+    }
+
+    public static double[] findAverages(int K, int[] arr) {
+        double[] result = new double[arr.length - K + 1];
+        int sum = 0;
+        int start = 0;
+        for (int end = 0; end < arr.length; end++) {
+            sum += arr[end];
+            if (arr.length - end <= K) {
+                result[start] = sum / (double) K;
+                sum -= arr[start];
+                start++;
+            }
+        }
+        return result;
+    }
+}
