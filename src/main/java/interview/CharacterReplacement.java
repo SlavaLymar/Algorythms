fbe2fc257e1628e29a57276bc527392a9b444acf
SlavaLymar <slavalymar@gmail.com>
2021-05-20 16:11:36 +0300

    commit from 20.05.21

--- src/main/java/interview/CharacterReplacement.java
+++ src/main/java/interview/CharacterReplacement.java
@@ -7,7 +7,20 @@ public class CharacterReplacement {
 
     // "a a b c c b b"
     public static int findLength(String str, int k) {
-        return 0;
+        int resultLength = 0, windowStart = 0, maxRepeatableLength = 0;
+        Map<Character, Integer> charInteger = new HashMap<>();
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char ch = str.charAt(windowEnd);
+            charInteger.put(ch, charInteger.getOrDefault(ch, 0) + 1);
+            maxRepeatableLength = Math.max(maxRepeatableLength, charInteger.get(ch));
+            if (windowEnd - windowStart + 1 - maxRepeatableLength > k) {
+                char deleteChar = str.charAt(windowStart);
+                charInteger.put(deleteChar, charInteger.get(deleteChar) - 1);
+                windowStart++;
+            }
+            resultLength = Math.max(resultLength, windowEnd - windowStart + 1);
+        }
+        return resultLength;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/interview/ReplacingOnes.java
@@ -0,0 +1,27 @@
+package interview;
+
+public class ReplacingOnes {
+
+    // 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1
+    public static int findLength(int[] arr, int k) {
+        int resultLength = 0, windowStart = 0, zeroCount = 0;
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            if (arr[windowEnd] == 0) {
+                zeroCount++;
+            }
+            if (zeroCount > k) {
+                if (arr[windowStart] == 0) {
+                    zeroCount--;
+                }
+                windowStart++;
+            }
+            resultLength = Math.max(resultLength, windowEnd - windowStart + 1);
+        }
+        return resultLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findLength(new int[]{0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1}, 2));
+        System.out.println(findLength(new int[]{0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1}, 3));
+    }
+}
