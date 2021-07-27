8bc43270f4a341cc425d9d860de4e6b98d7e3cde
Lymar Yaroslav Victorovich <Lymar.Y.Vi@sberbank.ru>
2021-07-27 21:30:16 +0300

    hackerrank Natera

---
+++ src/main/java/interview/Result.java
@@ -0,0 +1,24 @@
+package interview;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Result {
+
+    public static int minX(List<Integer> arr) {
+        int result = 0;
+        int sum = 0;
+        for (int x : arr) {
+            sum += x;
+            if (result > sum) {
+                result = sum;
+            }
+        }
+        return Math.abs(result) + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minX(Arrays.asList(new Integer[]{-5, 4, -2, 3, 1, -1, -6, -1, 0, 5})));
+    }
+
+}
---
+++ src/main/java/interview/Test.java
@@ -0,0 +1,20 @@
+package interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+
+
+    public static List<String> getArticleTitles(String author) {
+        List<String> result = new ArrayList<>();
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getArticleTitles("epaga"));
+    }
+}
