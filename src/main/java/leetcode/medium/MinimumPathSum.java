313078660b78e6bdbe072102b4cb195cf16c7c79
Lymar Yaroslav Victorovich <Lymar.Y.Vi@sberbank.ru>
2021-10-12 18:46:45 +0300

    12.10.21

--- src/main/java/leetcode/medium/MinimumPathSum.java
+++ src/main/java/leetcode/medium/MinimumPathSum.java
@@ -9,27 +9,24 @@ public class MinimumPathSum {
     // 2  6 8 7
     //
     // t: O (N ^ 2)
-    // space: O (N ^ 2)
+    // space: O (1)
     //
     public int minPathSum(int[][] grid) {
         if (grid == null) return 0;
         int m = grid.length;
         int n = grid[0].length;
-
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
         for (int i = 1; i < m; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
+            grid[i][0] = grid[i - 1][0] + grid[i][0];
         }
         for (int i = 1; i < n; i++) {
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
+            grid[0][i] = grid[0][i - 1] + grid[0][i];
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
             }
         }
-        return dp[m - 1][n - 1];
+        return grid[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
