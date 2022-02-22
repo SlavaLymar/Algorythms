05f6f673139fbb084bcefc9ff7294da85557f9e4
Lymar Yaroslav Victorovich <Lymar.Y.Vi@sberbank.ru>
2022-02-22 14:15:00 +0300

    22.02.22

---
+++ src/main/java/leetcode/easy/LinkedListCycle.java
@@ -0,0 +1,32 @@
+package leetcode.easy;
+
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    //           f
+    //      s
+    // 3 -> 2 -> 0 -> -4
+    //      ^__________|
+    //
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow.equals(fast)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
